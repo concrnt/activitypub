683644e71dc14eb362397477a0fa3f86a7f1deac
totegamma <github@gammalab.net>
2026-02-21 21:31:30 +0900

    implement actor

--- federation.ts
+++ federation.ts
@@ -1,4 +1,4 @@
-import { createFederation, Person } from "@fedify/fedify";
+import { createFederation, Follow, Person } from "@fedify/fedify";
 import { getLogger } from "@logtape/logtape";
 import { MemoryKvStore, InProcessMessageQueue } from "@fedify/fedify";
 
@@ -9,12 +9,26 @@ const federation = createFederation({
   queue: new InProcessMessageQueue(),
 });
 
+federation
+    .setInboxListeners("/users/{identifier}/inbox", "/inbox")
+    .on(Follow, async (ctx, follow) => {
+        if (follow.id == null || follow.actorId == null || follow.objectId == null) {
+            return;
+        }
+        const parsed = ctx.parseUri(follow.objectId);
+        if (parsed?.type !== "actor") return;
+        const follower = await follow.getActor(ctx);
+        console.debug(follower);
+    });
+
 federation.setActorDispatcher("/users/{identifier}", async (ctx, identifier) => {
-  return new Person({
-    id: ctx.getActorUri(identifier),
-    preferredUsername: identifier,
-    name: identifier,
-  });
+    return new Person({
+        id: ctx.getActorUri(identifier),
+        preferredUsername: identifier,
+        name: identifier,
+        url: new URL("/", ctx.url),
+        inbox: ctx.getInboxUri(identifier),
+    });
 });
 
 export default federation;
