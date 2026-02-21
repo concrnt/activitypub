24e846cc1e396d4081d21d9bd08669ef5162ce99
totegamma <github@gammalab.net>
2026-02-22 04:10:23 +0900

    add api endpoint

--- main.ts
+++ main.ts
@@ -1,13 +1,26 @@
+import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { behindProxy } from "x-forwarded-fetch";
 import federation from "./federation.ts";
 import "./logging.ts";
+import { federation as fedify } from "@fedify/hono";
+
+const app = new Hono();
+
+app.get("/api/test", (c) => {
+    return c.json({ message: "Hello from the API!" });
+});
+
+app.use(
+    "*",
+    fedify(federation, async(_c) => undefined),
+);
 
 serve(
   {
     port: 8000,
     fetch: behindProxy(
-      (req) => federation.fetch(req, { contextData: undefined }),
+        app.fetch
     ),
   },
   (info) =>
